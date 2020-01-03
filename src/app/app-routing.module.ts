d889c4a5e7808af444cdb35f2152e1fbfa39e7e2
Ali M'hammedi Alaoui <alimhammedialaoui@MacBook-Pro-de-Ali.local>
2020-01-04 00:15:11 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { UpdateUserComponent } from './update-user/update-user.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'user', pathMatch: 'full' },
+  {path:'users',component:UserListComponent},
+  {path:'users/add',component:CreateUserComponent},
+  { path: 'update/:matricule', component: UpdateUserComponent },
+  { path: 'details/:matricule', component: UserDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
